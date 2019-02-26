19d32b3eee15151539ebdcef844f1a19b81ba05d
Tilman Roeder <dyed.green.info@gmail.com>
2019-02-26 20:04:16 +0000

    Better error-code filter
    The error pages are now only served for html content type.

--- app.rb
+++ app.rb
@@ -15,18 +15,18 @@ class App < Views::Auth
     render "index.html.erb".to_sym
   end
 
-  get "/test" do
-    now = Time.new.to_i
-    day = 24 * 60 * 60
-    [1,50,20,4,2,7,11,0,0,5,2,1].each_with_index do |val, idx|
-      val.times do
-        $db.execute <<-SQL, [17, Time.at(now - day*20 - day*idx).to_i]
-          insert into url_hits (url, created) values (?, ?)
-        SQL
-      end
-    end
-    render "done!"
-  end
+  # get "/test" do
+  #   now = Time.new.to_i
+  #   day = 24 * 60 * 60
+  #   [1,50,20,4,2,7,11,0,0,5,2,1].each_with_index do |val, idx|
+  #     val.times do
+  #       $db.execute <<-SQL, [17, Time.at(now - day*20 - day*idx).to_i]
+  #         insert into url_hits (url, created) values (?, ?)
+  #       SQL
+  #     end
+  #   end
+  #   render "done!"
+  # end
 
   controller "/static/", Views::Static
   controller "/r/", Views::Refer
@@ -37,7 +37,7 @@ class App < Views::Auth
   controller "/", Views::Root # Provides special paths
   Views::Root << { pattern: "/", target: Views::User } # Provides login and account management
 
-  after status: (300..600) do
+  after status: (300..600), media_type: "text/html" do
     response.body = render "error.html.erb".to_sym
   end
 
